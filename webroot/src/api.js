37844dbe3c3db35bf034753ca08f6d4f4242b2a3
einargs <einargs@gmail.com>
2018-11-02 22:38:33 -0500

    Added axios and skeleton `api` utility.

---
+++ webroot/src/api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+// Return list of events
+function getEvents() {
+
+}
+
+// Return lone event
+function getEvent(eventId) {
+
+}
+
+// Return promise containing success message or error message.
+function postEvent(event) {
+
+}
+
+export default {
+  events: {
+    getList: getEvents,
+    add: postEvent,
+    getEvent: getEvent,
+  },
+  reports: {
+
+  },
+};
