03ba1312821d621cc1cd8443c7a6c48865f61b64
sremedios <sam.remedios@gmail.com>
2018-11-04 01:56:57 -0500

    modularizing files

---
+++ analytics/initialize_microstrategy_data.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import os
+import time
+from mstrio import microstrategy
+import json
+
+base_url="https://env-113279.customer.cloud.microstrategy.com/MicroStrategyLibrary/api"
+username="swr2u@mtmail.mtsu.edu"
+password="graei39lxn8"
+project_name="MicroStrategy Tutorial"
+
+DATA_DIR = "data"
+
+###########
+
+data = os.path.join(DATA_DIR, "quiet.csv")
+df = pd.read_csv(data, index_col=False)
+
+dataset_name = 'base_dataset'
+table_name = 'base_dataset_table'
+
+conn = microstrategy.Connection(base_url=base_url,
+                                username=username,
+                                password=password,
+                                project_name=project_name)
+conn.connect()
+
+print("Creating new base dataset on Microstrategy")
+dataset_id, new_table_id = conn.create_dataset(data_frame=df,
+                                               dataset_name=dataset_name,
+                                               table_name=table_name,
+                                               to_attribute=['work_id',
+                                                             'task_id'])
+
+with open(os.path.join(DATA_DIR, "base_dataset_ids.txt"), 'w') as f:
+    f.write(dataset_id + '\n' + new_table_id)
+
+###########
+
+'''
+
+data = os.path.join(DATA_DIR, "quiet.csv")
+df = pd.read_csv(data, index_col=False)
+
+dataset_name = 'pred_dataset'
+table_name = 'pred_dataset_table'
+
+conn = microstrategy.Connection(base_url=base_url,
+                                username=username,
+                                password=password,
+                                project_name=project_name)
+conn.connect()
+
+print("Creating new pred dataset on Microstrategy")
+dataset_id, new_table_id = conn.create_dataset(data_frame=df,
+                                               dataset_name=dataset_name,
+                                               table_name=table_name,
+                                               to_attribute=['work_id',
+                                                             'task_id'])
+
+with open(os.path.join(DATA_DIR, "pred_dataset_ids.txt"), 'w') as f:
+    f.write(dataset_id + '\n' + new_table_id)
+
+'''
--- analytics/server.py
+++ analytics/server.py
@@ -8,22 +8,22 @@ import json
 
 @post('/work')
 def get_new_work():
-    print(request.json)
-
-    # take json from request
-    # turn the list into a dict
-    # turn dict into pandas dataframe
-    # send to MS
-    # return success=happy
     
     df = pd.read_json(json.dumps(request.json), typ='series', convert_dates=False).to_frame().T
-    
+
     print("Adding to dataset on Microstrategy")
-    conn.update_dataset(data_frame=df, 
-                        update_policy='add', 
-                        dataset_id=dataset_id, 
-                        table_name=table_name, 
-                        table_id=new_table_id)
+    if len(df) == 11:
+        conn.update_dataset(data_frame=df, 
+                            update_policy='add', 
+                            dataset_id=dataset_id, 
+                            table_name=table_name, 
+                            table_id=new_table_id)
+    elif len(df) == 12:
+        conn.update_dataset(data_frame=df, 
+                            update_policy='add', 
+                            dataset_id=pred_dataset_id, 
+                            table_name=pred_table_name, 
+                            table_id=pred_new_table_id)
 
 
 base_url="https://env-113279.customer.cloud.microstrategy.com/MicroStrategyLibrary/api"
@@ -32,24 +32,26 @@ password="graei39lxn8"
 project_name="MicroStrategy Tutorial"
 
 DATA_DIR = "data"
+base_datasets_id_file = os.path.join(DATA_DIR, "base_datasets_ids.txt")
+table_name = "base_dataset_table"
+pred_datasets_id_file = os.path.join(DATA_DIR, "pred_datasets_ids.txt")
+pred_table_name = "pred_dataset_table"
+
 data = os.path.join(DATA_DIR, "quiet.csv")
 df = pd.read_csv(data, index_col=False)
 
-dataset_name = 'dataset_name'
-table_name = 'table_name'
+
+with open(base_datasets_id_file, 'r') as f:
+    dataset_id, new_table_id = f.readlines().split()
+
+with open(pred_datasets_id_file, 'r') as f:
+    pred_dataset_id, pred_new_table_id = f.readlines().split()
+
 
 conn = microstrategy.Connection(base_url=base_url,
                                 username=username,
                                 password=password,
                                 project_name=project_name)
 conn.connect()
-
-print("Creating new dataset on Microstrategy")
-dataset_id, new_table_id = conn.create_dataset(data_frame=df,
-                                               dataset_name=dataset_name,
-                                               table_name=table_name,
-                                               to_attribute=['work_id',
-                                                             'task_id'])
-
 
 run(host="localhost", port=8080, debug=True)
