d46f851ce00bbe74265f9e18ff0f371421856514
sremedios <sam.remedios@gmail.com>
2018-11-03 21:07:43 -0500

    server progress, can now receive stuff and try to update

--- analytics/server.py
+++ analytics/server.py
@@ -15,6 +15,41 @@ def get_new_work():
     # turn dict into pandas dataframe
     # send to MS
     # return success=happy
-    return "Hello world!"
+    
+    json_dict = request.json
+    df = pd.read_json(json_dict, convert_dates=False)
+    
+    print("Adding to dataset on Microstrategy")
+    conn.update_dataset(data_frame=df, 
+                        update_policy='add', 
+                        dataset_id=dataset_id, 
+                        table_name=table_name, 
+                        table_id=newTableId)
+
+
+base_url="https://env-113279.customer.cloud.microstrategy.com/MicroStrategyLibrary/api"
+username="swr2u@mtmail.mtsu.edu"
+password="graei39lxn8"
+project_name="MicroStrategy Tutorial"
+
+DATA_DIR = "data"
+data = os.path.join(DATA_DIR, "quiet.csv")
+df = pd.read_csv(data, index_col=False)
+
+dataset_name = 'dataset_name'
+table_name = 'table_name'
+
+conn = microstrategy.Connection(base_url=base_url,
+                                username=username,
+                                password=password,
+                                project_name=project_name)
+conn.connect()
+
+print("Creating new dataset on Microstrategy")
+dataset_id, new_table_id = conn.create_dataset(data_frame=df,
+                                               dataset_name=dataset_name,
+                                               table_name=table_name,
+                                               to_attribute=['task_id'])
+
 
 run(host="localhost", port=8080, debug=True)
--- analytics/toy_client.py
+++ analytics/toy_client.py
@@ -1,10 +1,17 @@
 import requests
+import os
 import json
+import pandas as pd
 
 ########### Testing launch game ###########  
 url = "http://localhost:8080"
 path = "/work"
-data = [{"id": "touhou_123"}, 
-        {"id": "melty_blood"}]
 
-r = requests.post(url+path, json=data,) 
+
+df = pd.read_json(os.path.join("data", "quiet.json"),
+                            convert_dates=False)
+
+df = df.iloc[0].to_json(date_unit='us', orient='index')# first row
+
+
+r = requests.post(url+path, json=df,) 
