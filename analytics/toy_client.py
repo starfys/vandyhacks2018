158d798e7cde3f0499e4d0ba7c552ad7b27cd0d6
sremedios <sam.remedios@gmail.com>
2018-11-03 22:36:37 -0500

    quick rename of task_id to work_id

--- analytics/toy_client.py
+++ analytics/toy_client.py
@@ -14,7 +14,7 @@ for _ in range(5):
 
     x += 1
 
-    df = json.dumps({"end_time":1541290912929.0,"music":0.0,"num_breaks":8.0,"num_interruptions":7.0,"num_meetings":1.0,"progress":0.6969148132,"start_time":1541290912671.0,"task_id":x,"workspace_volume":0.2105564469})
+    df = json.dumps({"end_time":1541290912929.0,"music":0.0,"num_breaks":8.0,"num_interruptions":7.0,"num_meetings":1.0,"progress":0.6969148132,"start_time":1541290912671.0,"work_id":x,"workspace_volume":0.2105564469})
     r = requests.post(url+path, json=df,) 
 
     input()
